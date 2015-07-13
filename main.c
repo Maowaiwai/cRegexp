19306a69d6f489eec8b1b4f81019e1f74aeb5c82
Maowaiwai <chenxwzcc@sina.com>
2015-07-13 07:08:04 -0700

    modify function split_string

--- main.c
+++ main.c
@@ -3,81 +3,99 @@
 #include <string.h>
 #include <malloc.h>
 
-int split_string(char *pstr)
+int find_string(char *target, char *pattern)
+{
+    return 0;
+}
+
+int split_string(char *target, char *regexp)
 {
     int i = 0;
+    int j = 0;
     char cc;
-    char *temp = pstr;
+    char *temp = regexp;
     char *ss[20];
     int clen1 = 0, clen2 = 0;
     int start1 = 0, end1 = 0;
     int start2 = 0, end2 = 0;
     char flag1 = 0, flag2 = 0;
+    long value = 0;
 
     for(i = 0; i < 20; i++)
         ss[i] = NULL;
 
-    printf("target string=%s\n", pstr);
-    printf("temp string=%s\n", temp);
-
     i = 0;
     while(*temp != '\0')
     {
-        cc = *temp;
+        j++;
         clen1++;
         clen2++;
-        switch(cc)
+        switch(*temp)
         {
             case '{' :
-                   start1 = clen1 - 1;
-                   flag1++;
-                   printf("find { at %d\n", start1);
-                   break;
+                    start1 = clen1 - 1;
+                    flag1++;
+                    printf("find { at %d\n", start1);
+                    break;
             case '}' :
-                   end1 = clen1 - 1;
-                   if(flag1 != 0)
-                   {
-                       flag1--;
-                       printf("find } at %d,len=%d\n", end1, end1 - start1 - 1);
-                       ss[i] = malloc(end1 - start1 + 2);
-                       strncpy(ss[i++], pstr + start1 - 1, end1 - start1 + 2);
-                   }
-                   else
-                   {
-                       printf("find } at %d, but no {\n", end1);
-                       clen1 = start1 = end1 = 0;
-                   }
-                   break;
+                    end1 = clen1 - 1;
+                    if(flag1 != 0)
+                    {
+                        flag1--;
+                        printf("find } at %d,len=%d\n", end1, end1 - start1 - 1);
+                        ss[i] = malloc(end1 - start1 + 2);
+                        strncpy(ss[i++], regexp + start1 - 1, end1 - start1 + 2);
+                        value = atol(ss[i - 1] + 2);
+                        printf("value=%ld\n", value);
+                    }
+                    else
+                    {
+                        printf("find } at %d, but no {\n", end1);
+                        clen1 = start1 = end1 = 0;
+                    }
+                    break;
             case '[' :
-                   start2 = clen2 - 1;
-                   flag2++;
-                   printf("find [ at %d\n", start2);
-                   break;
+                    start2 = clen2 - 1;
+                    flag2++;
+                    printf("find [ at %d\n", start2);
+                    break;
             case ']' :
-                   end2 = clen2 - 1;
-                   if(flag2 != 0)
-                   {
-                       flag2--;
-                       printf("find ] at %d,len=%d\n", end2, end2 - start2 - 1);
-                       ss[i] = malloc(end2 - start2 + 2);
-                       strncpy(ss[i++], pstr + start2 - 1, end2 - start2 + 2);
-                   }
-                   else
-                   {
-                       printf("find ] at %d, but no [\n", end2);
-                       clen2 = start2 = end2 = 0;
-                   }
-                   break;
+                    end2 = clen2 - 1;
+                    if(flag2 != 0)
+                    {
+                        flag2--;
+                        printf("find ] at %d,len=%d\n", end2, end2 - start2 - 1);
+                        ss[i] = malloc(end2 - start2 + 2);
+                        strncpy(ss[i++], regexp + start2 - 1, end2 - start2 + 2);
+                    }
+                    else
+                    {
+                        printf("find ] at %d, but no [\n", end2);
+                        clen2 = start2 = end2 = 0;
+                    }
+                    break;
             case '*' :
-                   break;
+                    printf("find * at %d\n", j);
+                    ss[i] = malloc(2);
+                    strncpy(ss[i++], temp - 1, 2);
+                    break;
             case '|' :
-                   break;
+                    printf("find | at %d\n", j);
+                    ss[i] = malloc(3);
+                    strncpy(ss[i++], temp - 1, 3);
+                    break;
             case '?' :
-                   break;
+                    printf("find ? at %d\n", j);
+                    ss[i] = malloc(2);
+                    strncpy(ss[i++], temp - 1, 2);
+                    break;
             case '+' :
-                   break;
+                    printf("find + at %d\n", j);
+                    ss[i] = malloc(2);
+                    strncpy(ss[i++], temp - 1, 2);
+                    break;
             default :
-                   break;
+                    break;
         }
         temp++;
     }
@@ -99,7 +117,11 @@ int main(int argc, char *argv[])
     for(i = 0; i < argc; i++)
         printf("argv[%d]=%s\n", i, argv[i]);
 
-    split_string(argv[1]);
+    if(argc == 3)
+        split_string(argv[1], argv[2]);
 
     return 0;
 }
+
+
+
